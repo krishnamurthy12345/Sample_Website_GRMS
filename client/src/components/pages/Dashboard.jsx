fe1542b322060210c717ecb68bf4fa99dfedb174
krishnamurthy12345 <murthykichu@gmail.com>
2025-02-24 17:17:31 +0530

    bug

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -8,17 +8,10 @@
 
 // imports
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 import withAuth from '../withAuth';
 
 const Dashboard = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const token = queryParams.get('token');
 
-  if (token) {
-    localStorage.setItem('token', token); 
-  }
   return (
     <div>Dashboard</div>
   )
